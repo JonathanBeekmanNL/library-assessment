a7c16a23e585f3ef96ed44dc7fb20a9bbfc0622b
Beekman <Jonathan.Beekman@rabobank.nl>
2020-11-06 13:49:21 +0100

    add 7 books restriction

--- src/main/kotlin/nl/rstraub/library/assessment/Member.kt
+++ src/main/kotlin/nl/rstraub/library/assessment/Member.kt
@@ -4,6 +4,7 @@ class Member(val name: String) {
     private val books = mutableListOf<String>()
 
     infix fun add(title: String) {
+        require(books.size < 7) {"member can loan a maximum of 7 books"}
         books.add(title)
     }
 
--- src/test/kotlin/nl/rstraub/library/assessment/MemberTest.kt
+++ src/test/kotlin/nl/rstraub/library/assessment/MemberTest.kt
@@ -1,10 +1,7 @@
 package nl.rstraub.library.assessment
 
 import assertk.assertThat
-import assertk.assertions.contains
-import assertk.assertions.hasSize
-import assertk.assertions.isEmpty
-import assertk.assertions.isEqualTo
+import assertk.assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -36,5 +33,13 @@ internal class MemberTest {
 
         assertThat(result).contains(book)
         assertThat(result).hasSize(1)
+    }
+
+    @Test
+    internal fun `should not add book if member has seven or more books`() {
+        val books = listOf("1", "2", "3", "4", "5", "6", "7")
+        books.forEach { book: String -> member add book }
+
+        assertThat { member add "8" }.isFailure()
     }
 }

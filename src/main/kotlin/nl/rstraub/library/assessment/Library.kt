517cb84400d560429d31b5f804e9ae3a1326a916
Mike van Viegen <Mike.van.Viegen@rabobank.nl>
2020-11-06 10:16:35 +0100

    Added members to construct parameters

--- src/main/kotlin/nl/rstraub/library/assessment/Library.kt
+++ src/main/kotlin/nl/rstraub/library/assessment/Library.kt
@@ -1,3 +1,6 @@
 package nl.rstraub.library.assessment
 
-class Library(val inventory: List<String>)
+class Library(
+    val inventory: List<String>,
+    val members: List<String>
+)
--- src/test/kotlin/nl/rstraub/library/assessment/LibraryTest.kt
+++ src/test/kotlin/nl/rstraub/library/assessment/LibraryTest.kt
@@ -11,16 +11,32 @@ internal class LibraryTest {
     inner class Inventory {
         @Test
         internal fun `should return empty list given no books`() {
-            val result = Library(emptyList()).inventory
+            val result = Library(emptyList(), emptyList()).inventory
             assertThat(result).isEmpty()
         }
 
         @Test
-        internal fun `should provide overview of the books`() {
+        internal fun `should return inventory given books`() {
             val books = listOf("bookA")
-            val result = Library(books).inventory
+            val result = Library(books, emptyList()).inventory
 
             assertThat(result).isEqualTo(books)
+        }
+    }
+
+    @Nested
+    inner class Members {
+        @Test
+        internal fun `should return empty list given no members`() {
+            val result = Library(emptyList(), emptyList()).members
+            assertThat(result).isEmpty()
+        }
+
+        @Test
+        internal fun `should return list of members given members`() {
+            val members = listOf("lidnr1")
+            val result = Library(emptyList(), members).members
+            assertThat(result).isEqualTo(members)
         }
     }
 }
